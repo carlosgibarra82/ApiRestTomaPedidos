bf1ae5e16de45b59558c5571e1189da4bf216ddd
carlosgibarra82 <carlosgibarra@gmail.com>
2018-06-02 13:19:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,42 +8,40 @@ const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 1234
 
 let users = [
-    {username: 'Leidy', password: '123456'},
-    {username: 'Carlos', password: '123456'}
+	{username: 'admin', password: '123'}
 ];
 
-var users1 = [{username: 'carlos', password: '123456'}];
+let devices = [
+	{name: 'tablet1', id: '1'},
+	{name: 'tablet2', id: '2'},
+	{name: 'tablet3', id: '3'},
+	{name: 'tablet4', id: '4'},
+	{name: 'tablet5', id: '5'}				  
+];
 
-let devices = [{name: 'tablet1', id: '1'},
-	       {name: 'tablet2', id: '2'},
-	       {name: 'tablet3', id: '3'},
-	       {name: 'tablet4', id: '4'},
-	       {name: 'tablet5', id: '5'}
-				  
-				  ];
 let dishes = [
-				{name: 'Bandeja Paisa', picture: 'https://image.ibb.co/j97vGJ/Bandeja_Paisa.jpg', ingredients: ['Arroz blanco','Carne de res en polvo y sudada o asada','Chicharrón','Huevo Frito','Tajadas de Platano maduro','Chorizo Antioqueño con limón','Arepa Antioqueña','Hogao','Fríjoles Cargamanto','Tomate Rojo en Rodajas','Aguacate'], cook_time: '15 Minutos', price: '25000'},
-				{name: 'Frito Pastuso', picture: 'https://image.ibb.co/meTD3y/fritopastuso.jpg', ingredients: ['Carne de cerdo','Ajo','Cebolla larga','Condimento trisasón','Pimentón', 'Sal al Gusto'], cook_time: '30 Minutos', price: '18000'},
-				{name: 'Cuy', picture: 'https://image.ibb.co/jaEWqd/cuy.jpg', ingredients: ['Cuy Entero Eviscerado','Papa Negra','Sal al gusto'], cook_time: '30 Minutos', price: '35000'},
-				{name: 'Lomo de Cerdo', picture: 'https://image.ibb.co/kc3rqd/lomo_de_cerdo.jpg', ingredients: ['3 dientes de ajo picados','cucharada de romero seco', 'sal y pimienta al gusto','1 kg de lomo de cerdo','¼ de taza (65 ml) de aceite de oliva','½ taza (125 ml) de vino blanco'], cook_time: '13 Minutos', price: '16000'},
-				{name: 'Lasagna', picture: 'https://image.ibb.co/fgfROy/lasagna.jpg', ingredients: ['400 g. de carne picada de cerdo','Queso rallado','Aceite de oliva virgen extra','Sofrito de Tomate y Cebolla','Pollo desmechado','Placas de Lasaña','Salsa Bechamel','Vino blanco','Sal al gusto'], cook_time: '15 Minutos', price: '15000'},
-				{name: 'Porción Pizza Hawaiana', picture: 'https://image.ibb.co/cr0t3y/pizza.jpg', ingredients: ['Masa de Harina','Queso','Piña en trocitos','Rodajas de Peperoni','Jamón'], cook_time: '15 Minutos', price: '8000'},
-				{name: 'Hamburguesa', picture: 'https://image.ibb.co/d6m0iy/hamburguesa.jpg', ingredients: ['tomate','cebolla','pimiento rojo','aceitunas verdes (sin hueso)','especias para aliñar (pimienta, eneldo, romero, orégano y tomillo)','pan rallado','huevo','queso','pepinillo agridulce','cebolla caramelizada','salsa BigBurguer Choví'], cook_time: '12 Minutos', price: '13500'},
-				{name: 'Camarones Apanados', picture: 'https://image.ibb.co/nMFhwJ/camarones_apanados.jpg', ingredients: ['1 Lb camarones medianos o grandes limpios y desvenados','250 gr ralladura de pan','Sal al gusto'], cook_time: '20 Minutos', price: '12000'},
-				{name: 'Corvina Apanada', picture: 'https://image.ibb.co/egB0iy/corbina_apanada.jpg', ingredients: ['Filete de corvina en trozos','Jugo de limón'], cook_time: '15 Minutos', price: '11000'},
-				{name: 'Cazuela de mariscos', picture: 'https://image.ibb.co/nPLt3y/cazuela_de_mariscos.jpg', ingredients: ['Mariscos','Pimentón Verde','Pimentón Rojo','Ajo','Caldo de pescado'], cook_time: '30 Minutos', price: '20000'},
-				{name: 'Langostinos al ajillo', picture: 'https://image.ibb.co/ijKxVd/Langostinos_al_ajillo.jpg', ingredients: ['1kg de langostinos','Limón al gusto','Perejil al gusto','Sal al gusto','Ajo al gusto'], cook_time: '25 minutos', price: '22000'},
-				{name: 'Lomo Hawaiano', picture: 'https://image.ibb.co/g09xVd/lomo_hawaiano.jpg', ingredients: ['Cerdo en trozos','Salsa Inglesa','Salsa de soya','Piña','Queso','Jamón','Cebolla','Miel de abeja','Catsup'], cook_time: '15 Minutos', price: '17500'},
-				{name: 'Tacos', picture: 'https://image.ibb.co/eO5t3y/tacos.jpg', ingredients: ['Tortillas de harina','Carne Picada','Aceite de Maíz','Queso Chedar Rallado','Tomate','Cebolla Verde','Lechuga','Salsa Mexicana'], cook_time: '15 minutes', price: '10500'},
-				{name: 'Sancocho de Gallina', picture: 'https://image.ibb.co/cW7HVd/Sancocho_de_Gallina.jpg', ingredients: ['Muslos de Pollo','Papa Pastusa','Yuca','Platano','Cebolla','Cilantro','Mazorca','Ajo','Sal al Gusto'], cook_time: '2 a 3 horas', price: '15000'},
-				{name: 'Sancocho de Espinazo', picture: 'https://image.ibb.co/hzxjAd/Sancocho_de_Espinazo.png', ingredients: ['Carne de Cerdo','Platano verde','Papa Pelada','Papa Criolla','Mazorca','Cebolla','Cilantro','Zanahoria','Sal al gusto'], cook_time: '1 Hora', price: '15000'},
-				{name: 'Sopa de Mondongo', picture: 'https://image.ibb.co/gAL4Ad/Sopa_de_Mondongo.jpg', ingredients: ['Callos de Res','Jugo de Limón','Carne de Cerdo','Chorizos Colombianos','Tomate','Cebolla Blanca','Papas peladas','Yuca','Sal al gusto'], cook_time: '1 Hora', price: '8000'},
-				{name: 'Gallina Campesina', picture: 'https://image.ibb.co/b9FFGJ/Gallina_Campesina.jpg', ingredients: ['Pollo troceado','Tomate','Cebolla','Cilantro','Ajo','Papa','Yuca','Sal','Pimienta'], cook_time: '2 Horas', price: '16000'},
-				{name: 'Carne a la plancha', picture: 'https://image.ibb.co/fJPLiy/Carne_a_la_plancha.jpg', ingredients: ['Filetes de Carne','Ajo','Cebolla','Pimentones','Sal y Pimienta'], cook_time: '30 Minutos', price: '12500'},
-				{name: 'Salchipapas', picture: 'https://image.ibb.co/e1dfiy/Salchipapas.jpg', ingredients: ['Papas A la Francesa','Salchichas','Salsa de Tomate','Mayonesa','Sal al gusto'], cook_time: '10 Minutos', price: '7500'},
-				{name: 'Lomo a caballo', picture: 'https://image.ibb.co/cNYPAd/Lomo_a_caballo.jpg', ingredients: ['Carne de Res','Tomate','Perejil','Huevo','Platano','Yuca','Sal al gusto'], cook_time: '20 Minutos', price: '14000'},
-				{name: 'Cerdillo', picture: 'https://image.ibb.co/ckfFGJ/Cerdillo.jpg', ingredients: ['Bondiola de Cerdo','Limon','Ajo','Tomillo Seco','Sal y Pimienta'], cook_time: '45 Minutos', price: '20000'}
-		];
+	{name: 'Bandeja Paisa', picture: 'https://image.ibb.co/j97vGJ/Bandeja_Paisa.jpg', ingredients: ['Arroz blanco','Carne de res en polvo y sudada o asada','Chicharrón','Huevo Frito','Tajadas de Platano maduro','Chorizo Antioqueño con limón','Arepa Antioqueña','Hogao','Fríjoles Cargamanto','Tomate Rojo en Rodajas','Aguacate'], cook_time: '15 Minutos', price: '25000'},
+	{name: 'Frito Pastuso', picture: 'https://image.ibb.co/meTD3y/fritopastuso.jpg', ingredients: ['Carne de cerdo','Ajo','Cebolla larga','Condimento trisasón','Pimentón', 'Sal al Gusto'], cook_time: '30 Minutos', price: '18000'},
+	{name: 'Cuy', picture: 'https://image.ibb.co/jaEWqd/cuy.jpg', ingredients: ['Cuy Entero Eviscerado','Papa Negra','Sal al gusto'], cook_time: '30 Minutos', price: '35000'},
+	{name: 'Lomo de Cerdo', picture: 'https://image.ibb.co/kc3rqd/lomo_de_cerdo.jpg', ingredients: ['3 dientes de ajo picados','cucharada de romero seco', 'sal y pimienta al gusto','1 kg de lomo de cerdo','¼ de taza (65 ml) de aceite de oliva','½ taza (125 ml) de vino blanco'], cook_time: '13 Minutos', price: '16000'},
+	{name: 'Lasagna', picture: 'https://image.ibb.co/fgfROy/lasagna.jpg', ingredients: ['400 g. de carne picada de cerdo','Queso rallado','Aceite de oliva virgen extra','Sofrito de Tomate y Cebolla','Pollo desmechado','Placas de Lasaña','Salsa Bechamel','Vino blanco','Sal al gusto'], cook_time: '15 Minutos', price: '15000'},
+	{name: 'Porción Pizza Hawaiana', picture: 'https://image.ibb.co/cr0t3y/pizza.jpg', ingredients: ['Masa de Harina','Queso','Piña en trocitos','Rodajas de Peperoni','Jamón'], cook_time: '15 Minutos', price: '8000'},
+	{name: 'Hamburguesa', picture: 'https://image.ibb.co/d6m0iy/hamburguesa.jpg', ingredients: ['tomate','cebolla','pimiento rojo','aceitunas verdes (sin hueso)','especias para aliñar (pimienta, eneldo, romero, orégano y tomillo)','pan rallado','huevo','queso','pepinillo agridulce','cebolla caramelizada','salsa BigBurguer Choví'], cook_time: '12 Minutos', price: '13500'},
+	{name: 'Camarones Apanados', picture: 'https://image.ibb.co/nMFhwJ/camarones_apanados.jpg', ingredients: ['1 Lb camarones medianos o grandes limpios y desvenados','250 gr ralladura de pan','Sal al gusto'], cook_time: '20 Minutos', price: '12000'},
+	{name: 'Corvina Apanada', picture: 'https://image.ibb.co/egB0iy/corbina_apanada.jpg', ingredients: ['Filete de corvina en trozos','Jugo de limón'], cook_time: '15 Minutos', price: '11000'},
+	{name: 'Cazuela de mariscos', picture: 'https://image.ibb.co/nPLt3y/cazuela_de_mariscos.jpg', ingredients: ['Mariscos','Pimentón Verde','Pimentón Rojo','Ajo','Caldo de pescado'], cook_time: '30 Minutos', price: '20000'},
+	{name: 'Langostinos al ajillo', picture: 'https://image.ibb.co/ijKxVd/Langostinos_al_ajillo.jpg', ingredients: ['1kg de langostinos','Limón al gusto','Perejil al gusto','Sal al gusto','Ajo al gusto'], cook_time: '25 minutos', price: '22000'},
+	{name: 'Lomo Hawaiano', picture: 'https://image.ibb.co/g09xVd/lomo_hawaiano.jpg', ingredients: ['Cerdo en trozos','Salsa Inglesa','Salsa de soya','Piña','Queso','Jamón','Cebolla','Miel de abeja','Catsup'], cook_time: '15 Minutos', price: '17500'},
+	{name: 'Tacos', picture: 'https://image.ibb.co/eO5t3y/tacos.jpg', ingredients: ['Tortillas de harina','Carne Picada','Aceite de Maíz','Queso Chedar Rallado','Tomate','Cebolla Verde','Lechuga','Salsa Mexicana'], cook_time: '15 minutes', price: '10500'},
+	{name: 'Sancocho de Gallina', picture: 'https://image.ibb.co/cW7HVd/Sancocho_de_Gallina.jpg', ingredients: ['Muslos de Pollo','Papa Pastusa','Yuca','Platano','Cebolla','Cilantro','Mazorca','Ajo','Sal al Gusto'], cook_time: '2 a 3 horas', price: '15000'},
+	{name: 'Sancocho de Espinazo', picture: 'https://image.ibb.co/hzxjAd/Sancocho_de_Espinazo.png', ingredients: ['Carne de Cerdo','Platano verde','Papa Pelada','Papa Criolla','Mazorca','Cebolla','Cilantro','Zanahoria','Sal al gusto'], cook_time: '1 Hora', price: '15000'},
+	{name: 'Sopa de Mondongo', picture: 'https://image.ibb.co/gAL4Ad/Sopa_de_Mondongo.jpg', ingredients: ['Callos de Res','Jugo de Limón','Carne de Cerdo','Chorizos Colombianos','Tomate','Cebolla Blanca','Papas peladas','Yuca','Sal al gusto'], cook_time: '1 Hora', price: '8000'},
+	{name: 'Gallina Campesina', picture: 'https://image.ibb.co/b9FFGJ/Gallina_Campesina.jpg', ingredients: ['Pollo troceado','Tomate','Cebolla','Cilantro','Ajo','Papa','Yuca','Sal','Pimienta'], cook_time: '2 Horas', price: '16000'},
+	{name: 'Carne a la plancha', picture: 'https://image.ibb.co/fJPLiy/Carne_a_la_plancha.jpg', ingredients: ['Filetes de Carne','Ajo','Cebolla','Pimentones','Sal y Pimienta'], cook_time: '30 Minutos', price: '12500'},
+	{name: 'Salchipapas', picture: 'https://image.ibb.co/e1dfiy/Salchipapas.jpg', ingredients: ['Papas A la Francesa','Salchichas','Salsa de Tomate','Mayonesa','Sal al gusto'], cook_time: '10 Minutos', price: '7500'},
+	{name: 'Lomo a caballo', picture: 'https://image.ibb.co/cNYPAd/Lomo_a_caballo.jpg', ingredients: ['Carne de Res','Tomate','Perejil','Huevo','Platano','Yuca','Sal al gusto'], cook_time: '20 Minutos', price: '14000'},
+	{name: 'Cerdillo', picture: 'https://image.ibb.co/ckfFGJ/Cerdillo.jpg', ingredients: ['Bondiola de Cerdo','Limon','Ajo','Tomillo Seco','Sal y Pimienta'], cook_time: '45 Minutos', price: '20000'}
+];
 let drinks = [			
 	{type: 'jugoenagua', flavor: 'Tomate'},
 	{type: 'jugoenagua', flavor: 'Piña'},
@@ -82,14 +80,14 @@ let drinks = [
 	{type: 'alcoholicas', flavor: 'Vodka'}
 ];
 
-let order = 	[
-			{
-				user:'Leidy',
-				device:'tablet1',
-				dishes:['Bandeja Paisa','Cuy'],
-				drinks:['Gaseosa','Cerveza']
-			}
-		]
+let order = [
+	{
+		user:'Leidy',
+		device:'tablet1',
+		dishes:['Bandeja Paisa','Cuy'],
+		drinks:['Gaseosa','Cerveza']
+	}
+];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
